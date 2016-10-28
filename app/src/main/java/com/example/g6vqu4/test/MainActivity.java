aaddc20c9d9637c683695c09487b75cfae9bcea6
= <ria8246@gmail.com>
2016-10-28 09:10:52 +0200

    init + MasikActivity

---
+++ app/src/main/java/com/example/g6vqu4/test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.g6vqu4.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //lefut mikor elindul
+        setContentView(R.layout.activity_main); //beállítja a tartalmat
+    }
+}
