93aa2ba44dea953579170978d15b6e3829e5fef5
= <ria8246@gmail.com>
2016-10-28 10:26:57 +0200

    something

--- app/src/main/java/com/example/g6vqu4/test/MainActivity.java
+++ app/src/main/java/com/example/g6vqu4/test/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.g6vqu4.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //lefut mikor elindul
         setContentView(R.layout.activity_main); //beállítja a tartalmat
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+                                      @Override
+                                      public void onClick(View view) {
+                                          Intent intentPeldany = new Intent(getApplicationContext(), MasikActivity.class);
+                                          intentPeldany.putExtra("g6vqu4.talkingandroid.MESSAGE", "Hello masik activitx");
+                                          startActivity(intentPeldany);
+                                      }
+                                  }
+
+        );
+
     }
 }
--- app/src/main/java/com/example/g6vqu4/test/MasikActivity.java
+++ app/src/main/java/com/example/g6vqu4/test/MasikActivity.java
@@ -1,7 +1,10 @@
 package com.example.g6vqu4.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MasikActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MasikActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masik);
+    }
+
+    Bundle extras = getIntent().getExtras();
+    if(extras != null){
+        String message = extras.getString();
+        TextView MessageTextView = (TextView) findViewById(R.id.Textview4);
+    }
+
+
+    protected void Katt (View view){
+        Intent intentPeldany = new Intent(getApplicationContext(),MasikActivity.class);
+        in
+        startActivity(intentPeldany);
     }
 }
